9df07a5f2126061edc857daf26228a721e57611c
Rlk81 <33742619+Rlk81@users.noreply.github.com>
2019-03-13 22:27:16 -0500

    Add files via upload

--- PaintColor.java
+++ PaintColor.java
@@ -5,21 +5,21 @@ package cardec;
 public class PaintColor extends CarOptions {
 	public String color ;
 
-	public PaintColor(ICar car) {
-		super(car);
+	public PaintColor(ICar c) {
+		super(c);
 		System.out.print("  PaintColor Constructor\n");
 		this.color = "black";
 	}
 	
 	public PaintColor(ICar c, String col) {
-		super(car);
+		super(c);
 		//System.out.print("  PaintColor Constructor\n");
 		this.color = col;
 	}
 	
 	 @Override
     public String GetDescription(){
-        return super.GetDescription + "My new " + color + " car make me the slickest person around.";
+        return super.GetDescription() + "My new " + color + " car make me the slickest person around.";
     }
 	
 }
