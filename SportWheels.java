bfc0bfdf8aa7dd871c8e8a55875ba36a0181b26d
Isaac van Til <iwv1@txstate.edu>
2019-03-13 21:14:52 -0500

    Add files via upload

---
+++ SportWheels.java
@@ -0,0 +1,18 @@
+package cardec;
+
+public class SportWheels extends CarOptions  {
+	
+    public SportWheels(ICar car){
+        super(car);
+        System.out.print(" SportWheels Constructor\n");
+    }
+
+    @Override
+    public String GetDescription(){
+        return super.GetDescription() + addSportWheels;
+    }
+  	
+    private string addSportWheels(){
+        return " with some super cool sport wheels."
+    }
+}
