1eac1666429d9f6cc9e5780d27f7989ad009784e
FredHanson <47045984+FredHanson@users.noreply.github.com>
2019-03-13 21:08:49 -0500

    Update StripeColor.java

--- StripeColor.java
+++ StripeColor.java
@@ -6,14 +6,15 @@ public class StripeColor extends CarOptions
 
 	public StripeColor(ICar c, String col) {
 		super(c);
-		System.out.print("  StripeColor Constructor\n");
+		//System.out.print("  StripeColor Constructor\n");
 		this.color = col;
 	}
 
 	@Override
-	public void GetDescription(){
-		super.GetDescription();
-		System.out.print(" \n  My stripe's color is " + color + ". ");
-		System.out.print("My " + color + " stripe lets me drive very fast.");
+	public String GetDescription(){
+		return super.GetDescription + " \n  My stripe's color is " + color + ". My " + color + " stripe lets me drive very fast.";
+		//super.GetDescription();
+		//System.out.print(" \n  My stripe's color is " + color + ". ");
+		//System.out.print("My " + color + " stripe lets me drive very fast.");
 	}
 }
